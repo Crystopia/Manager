08a5e5b2b4f7b3a8f8c5fbf626b7367f54883068
Jesper <94371018+jesforge@users.noreply.github.com>
2025-02-18 07:49:55 +0100

    Update ChatEvent.kt

--- velocity/src/main/kotlin/me/jesforge/proxymanager/events/ChatEvent.kt
+++ velocity/src/main/kotlin/me/jesforge/proxymanager/events/ChatEvent.kt
@@ -18,38 +18,39 @@ class ChatEvent {
         val player = event.player
         val playerData = ConfigManager.player.players[player.uniqueId.toString()]
         val message = event.message
+
+        if (playerData == null) {
+            return  // No player data, so no processing
+        }
+
         val formattedMessage: Component = mm.deserialize("${getLuckPermsPrefix(player)}: $message")
-
-        if (playerData!!.chatMode == ChatModeType.NONE || playerData!!.chatNotify == ChatNoitfiyType.NONE) {
+        
+        if (playerData.chatMode == ChatModeType.NONE || playerData.chatNotify == ChatNoitfiyType.NONE) {
             player.sendMessage(mm.deserialize("<color:#f8ffd4>You are in mute mode and cannot see or write messages. <gray>Use /chat to change your mode.</gray></color>"))
-            return@onChatEvent
+            return
         }
 
-        if (playerData!!.chatMode == ChatModeType.ALL || playerData!!.chatNotify == ChatNoitfiyType.ALL) {
-
-
-            Main.instance.server.allPlayers.forEach { players ->
-                ConfigManager.player.players.forEach { player ->
-                    if (player.value.chatNotify == ChatNoitfiyType.ALL || player.value.chatMode == ChatModeType.ALL) {
-                        players.sendMessage(formattedMessage)
-                    } else {
-                    }
-                }
-            }
-        } else if (playerData!!.chatMode == ChatModeType.SERVER || playerData!!.chatNotify == ChatNoitfiyType.SERVER) {
-
-            Main.instance.server.allPlayers.forEach { players ->
-                ConfigManager.player.players.forEach { player ->
-                    if (player.value.chatMode == ChatModeType.SERVER || player.value.chatNotify == ChatNoitfiyType.SERVER) {
-                        players.sendMessage(formattedMessage)
-                    } else {
-
-                    }
-                }
-
-            }
+        val messageRecipients = when {
+            playerData.chatMode == ChatModeType.ALL || playerData.chatNotify == ChatNoitfiyType.ALL -> Main.instance.server.allPlayers
+            playerData.chatMode == ChatModeType.SERVER || playerData.chatNotify == ChatNoitfiyType.SERVER -> Main.instance.server.allPlayers
+            else -> return  // If no conditions match, don't send any messages
         }
 
+        messageRecipients.forEach { recipients ->
+            // Only send message to players with matching settings
+            if (shouldSendToPlayer(recipients)) {
+                recipients.sendMessage(formattedMessage)
+            }
+        }
+    }
 
+    private fun shouldSendToPlayer(player: Player): Boolean {
+        val playerData = ConfigManager.player.players[player.uniqueId.toString()]
+        return playerData != null && (
+                playerData.chatMode == ChatModeType.ALL || 
+                playerData.chatNotify == ChatNoitfiyType.ALL || 
+                playerData.chatMode == ChatModeType.SERVER || 
+                playerData.chatNotify == ChatNoitfiyType.SERVER
+        )
     }
 }
